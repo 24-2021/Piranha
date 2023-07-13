19051ebe7e2ac2b696c12536545cbc11ccc30a23
24 <75511051+mamba-2021@users.noreply.github.com>
2023-07-13 09:25:34 +0800

    Add files via upload

---
+++ Piranha.py
@@ -0,0 +1,128 @@
+import chardet
+import yaml
+import sys
+
+#读取配置文件
+def read_config_file(config_file):
+    with open(config_file, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+
+
+def check_files_for_keywords(config):
+    keywords = config['Keywords']
+    return keywords
+
+# 用法示例
+config_file = 'config.yaml'  # 替换为你的配置文件路径
+config = read_config_file(config_file)
+keys=check_files_for_keywords(config)
+
+
+
+#读取文件内容的函数
+def read_file_all(file_path,code):
+    try:
+        with open(file_path, 'r',encoding=code) as file:
+            content = file.read()
+        return content
+    except FileNotFoundError:
+        return "文件未找到"
+    except IOError:
+        return "读取文件时发生错误"
+
+
+#获取文件的编码方式的函数
+def detect_encoding(file_path):
+    try:
+        with open(file_path, 'rb') as file:
+            raw_data = file.read()
+        result = chardet.detect(raw_data)
+        encoding = result['encoding']
+        confidence = result['confidence']
+        return encoding, confidence
+    except FileNotFoundError:
+        return None, "文件未找到"
+    except IOError:
+        return None, "读取文件时发生错误"
+
+
+def get_code(file_path):
+    encoding, confidence = detect_encoding(file_path)
+    if encoding is not None:
+        # print(f"检测到文件编码为 {encoding}，置信度为 {confidence}")
+        return encoding
+    else:
+        print("该文件无法读取")
+# code=get_code("G:\\tcsec-keeper\\sdk\\pom.xml" )
+
+#正式读取全部
+def file_name_list(txt):
+
+        f = open(txt,encoding='utf-8')
+        f = f.readlines()
+        for path in f:
+            try:
+                path = path.strip('\n')
+                path = path.strip('/')
+                code=get_code(path)
+                content=read_file_all(path,code)  #组装
+                # print(content)
+                poc_file = open('file_all.txt', 'a+')
+                poc_file.write(content + '\n')
+                poc_file.close()
+            except:
+                    print("读取异常")
+                    continue
+#正式读取全部
+def file_name_list_check(txt):
+
+        f = open(txt,encoding='utf-8')
+        f = f.readlines()
+        for path in f:
+            try:
+                path = path.strip('\n')
+                path = path.strip('/')
+                code=get_code(path)
+                content=read_file_all(path,code)  #组装
+                for key in keys:
+                # if  "jdbc:" in content or "redis" in content or "mysql" in content or "pgsql" in content or "postgresql" in content or "oracle" in content\
+                #     or "oss." in content or "oss.accessKey" in content or "oss.secretKey" in content or "secretKey" in content or "accessKey" in content \
+                #     or "ak" in content or "sk" in content  or "mongondb" in content or "secret" in content or "password" in content or "username" in content\
+                #     or "pass" in content or "user" in content or "pwd" in content or "mail" in content or "smtp" in content or "token" in content or "token" in content\
+                #     or "datasource" in content or "datasource" in content or "accessKeyId" in content or "accessKeySecret" in content or "sms" in content\
+                #     or "access" in content or "access" in content or "mssql" in content or "sqlserver" in content or "aliyun" in content:
+                    if key in content:
+                        print(content)
+                        poc_file = open('file_filterate.txt', 'a+')
+                        poc_file.write(content + '\n')
+                        poc_file.close()
+                        break
+            except:
+                    print("读取异常")
+                    continue
+def help():
+    print("python Piranha.py path.txt")
+    print("")
+    print("Linux：")
+    print("find / name *.config > path.txt")
+    print("./Piranha path.txt")
+    print("")
+    print("Windows：")
+    print("dir /s/b C:\*.config > path.txt")
+    print("Piranha.exe path.txt")
+    print("")
+    print("build：")
+    print("pyinsatller -F Piranha.py")
+
+if __name__ == '__main__' :
+    try:
+        cmd1=sys.argv[1]
+        file = file_name_list(cmd1)
+        print(file)
+        file1 = file_name_list_check(cmd1)
+        print(file1)
+    except:
+        help()
+
+
--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# Piranha
-内网配置文件快速收集
+## 简介
+
+在拿到webshell之后的收集该服务器上所有配置文件，极大节省了你手翻配置的时间，让你内网更快一步
+
+## 版本
+
+python >=3.1.x
+
+Piranha=v0.1
+
+## 安装
+
+```
+pip3 install -r re.txt
+```
+
+### 配置文件
+
+config.yaml
+
+![image-20230713092022703](C:\Users\apt\AppData\Roaming\Typora\typora-user-images\image-20230713092022703.png)
+
+可以根据个人实战情况，进行修改字典
+
+## 用法
+
+### Windows
+
+查找配置文件放到path.txt
+
+```
+dir /s/b C:\www\*.conf >path.txt  #这里配置文件后缀不仅限于conf，可根据实战环境进行搜索
+```
+
+进行搜索并且写入
+
+```
+Piranha_win_amd64.exe path.txt
+```
+
+查找完毕之后，会生成file_all.txt,file_filterate.txt；file_filterate.txt是根据字典过滤后的
+
+![image-20230713091713413](C:\Users\apt\AppData\Roaming\Typora\typora-user-images\image-20230713091713413.png)
+
+### Linux
+
+查找配置文件放到path.txt
+
+```
+find /var/www/ -name *.conf >path.txt  #这里配置文件后缀不仅限于conf，可根据实战环境进行搜索
+```
+
+进行搜索并且写入
+
+```
+./Piranha_lin_amd64 path.txt
+```
+
+### 打包
+
+```
+pip3 install pyinstaller
+pyinstaller -F Piranha.py
+```
+
