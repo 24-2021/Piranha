cc2d7c9db5cd7849fd9fa8b0ee32f4ee6955ca6a
24 <75511051+mamba-2021@users.noreply.github.com>
2023-07-24 15:21:07 +0800

    Add files via upload

--- Piranha.py
+++ Piranha.py
@@ -1,6 +1,7 @@
 import chardet
 import yaml
 import sys
+from cprint import cprint
 
 #读取配置文件
 def read_config_file(config_file):
@@ -24,7 +25,7 @@ keys=check_files_for_keywords(config)
 def read_file_all(file_path,code):
     try:
         with open(file_path, 'r',encoding=code) as file:
-            content = file.read()
+            content = file.readlines()
         return content
     except FileNotFoundError:
         return "文件未找到"
@@ -45,6 +46,8 @@ def detect_encoding(file_path):
         return None, "文件未找到"
     except IOError:
         return None, "读取文件时发生错误"
+    except Exception:
+        return None,"获取编码异常"
 
 
 def get_code(file_path):
@@ -53,74 +56,75 @@ def get_code(file_path):
         # print(f"检测到文件编码为 {encoding}，置信度为 {confidence}")
         return encoding
     else:
-        print("该文件无法读取")
+        cprint.fatal("该文件无法读取")
+        # print("该文件无法读取")
 # code=get_code("G:\\tcsec-keeper\\sdk\\pom.xml" )
+
+#获取编码格式并且将读取的文件转换成utf-8
 
 #正式读取全部
 def file_name_list(txt):
-
-        f = open(txt,encoding='utf-8')
+        f = open(txt)
         f = f.readlines()
         for path in f:
-            try:
-                path = path.strip('\n')
-                code=get_code(path)
-                content=read_file_all(path,code)  #组装
-                poc_file = open('file_all.txt', 'a+')
-                print("Read and write all successful")
-                poc_file.write(content + '\n')
-                poc_file.close()
-            except:
-                    print("读取异常")
-                    continue
+                try:
+                    path = path.strip('\n')
+                    code=get_code(path) #获取编码格式
+                    content=read_file_all(path,code)  #组装
+                    for con in content:
+                        poc_file = open('file_all.txt', 'a+')
+                        cprint.info(f"Read and write {path} all successful")
+                        # print(f"Read and write {path} all successful")
+                        poc_file.write(con + '\n')
+                        poc_file.close()
+                except:
+                        print("读取异常")
+                        continue
 #正式读取全部
 def file_name_list_check(txt):
 
-        f = open(txt,encoding='utf-8')
+        f = open(txt)
         f = f.readlines()
         for path in f:
             try:
                 path = path.strip('\n')
                 code=get_code(path)
+
                 content=read_file_all(path,code)  #组装
-                for key in keys:
-                # if  "jdbc:" in content or "redis" in content or "mysql" in content or "pgsql" in content or "postgresql" in content or "oracle" in content\
-                #     or "oss." in content or "oss.accessKey" in content or "oss.secretKey" in content or "secretKey" in content or "accessKey" in content \
-                #     or "ak" in content or "sk" in content  or "mongondb" in content or "secret" in content or "password" in content or "username" in content\
-                #     or "pass" in content or "user" in content or "pwd" in content or "mail" in content or "smtp" in content or "token" in content or "token" in content\
-                #     or "datasource" in content or "datasource" in content or "accessKeyId" in content or "accessKeySecret" in content or "sms" in content\
-                #     or "access" in content or "access" in content or "mssql" in content or "sqlserver" in content or "aliyun" in content:
-                    if key in content:
-                        poc_file = open('file_filterate.txt', 'a+')
-                        print("Read and write all check successful")
-                        poc_file.write(content + '\n')
-                        poc_file.close()
-                        break
+                for con in content:  #遍历我们读取的文件
+                    for key in keys: #遍历我们config.yaml
+                        if key in con:
+                            poc_file = open('file_filterate.txt', 'a+')
+                            cprint.ok(f"Read and write {path} all check successful")
+                            # print(f"Read and write {path} all check successful")
+                            poc_file.write(con + '\n')
+                            poc_file.close()
+                            break
             except:
-                    print("读取异常")
-                    continue
+                cprint.fatal("读取异常")
+                # print("读取异常")
+                continue
 def help():
-    print("python Piranha.py path.txt")
-    print("")
-    print("Linux：")
-    print("find / name *.config > path.txt")
-    print("./Piranha path.txt")
-    print("")
-    print("Windows：")
-    print("dir /s/b C:\*.config > path.txt")
-    print("Piranha.exe path.txt")
-    print("")
-    print("build：")
-    print("pyinsatller -F Piranha.py")
+    cprint.err("python Piranha.py path.txt")
+    cprint.err("")
+    cprint.err("Linux：")
+    cprint.err("find / name *.config > path.txt")
+    cprint.err("./Piranha path.txt")
+    cprint.err("")
+    cprint.err("Windows：")
+    cprint.err("dir /s/b C:\*.config > path.txt")
+    cprint.err("Piranha.exe path.txt")
+    cprint.err("")
+    cprint.err("build：")
+    cprint.err("pyinsatller -F Piranha.py")
 
 if __name__ == '__main__' :
     try:
         cmd1=sys.argv[1]
         file = file_name_list(cmd1)
-        print(file)
+        cprint.err(file)
         file1 = file_name_list_check(cmd1)
-        print(file1)
+        cprint.err(file1)
     except:
         help()
-
 
